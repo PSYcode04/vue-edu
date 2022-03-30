18381725ae6af03ee3cc3251f6c0175f9a437d9a
PSYcode04 <psy.code04@gmail.com>
2022-03-30 12:57:46 +0900

    ✨ Login Page vue-router #6

--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -2,13 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import MainPage from './pages/MainPage.vue'
+import LoginPage from './pages/LoginPage.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 
-const routes = [{
-  path: '/', component: MainPage
-}];
+const routes = [
+  {
+    path: '/', component: MainPage
+  },
+  {
+    path: '/login', component: LoginPage
+  }
+];
 
 const router = new VueRouter({
   routes
